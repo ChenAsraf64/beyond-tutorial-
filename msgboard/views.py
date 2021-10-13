5f1df7529b99e753bd19386b5e59b60b2371c7b2
Chen Asraf <chen.asraf@post.udc.ac.il>
2021-10-14 01:51:38 +0300

    Last tak

--- msgboard/views.py
+++ msgboard/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Message
 
-# Create your views here.
+
+def board(request):
+      messages = Message.objects.order_by('-date')
+      if request.method == "POST":
+          form = MessageForm(request.POST)
+          if form.is_valid():
+              form.save()
+              return redirect('board')
+      else:
+          form = MessageForm()
+      return render(request, 'msgboard/board.html', {
+          'messages': messages,
+          'form': form,
+      })
+
